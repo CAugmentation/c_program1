9d89d1a18cc3181208c0f899b07493d22dd62a37
Dejan Jankovic <dejanjankovic70@proton.me>
2025-06-21 10:30:28 +0200

    Add some explanatory comments

--- sample.c
+++ sample.c
@@ -11,12 +11,14 @@ int main(void)
     printf("Enter uppercase character: ");
     scanf("%c", &character);
 
+    // Check if the inputted character is uppercase.
     if (character < 'A' || character > 'Z')
     {
         printf("Non uppercase character inputted. Exiting.\n");
         return -1;
     }
 
+    // Flip the 6th bit of the lowercase character from 0 to 1.
     printf("Lowercase character: %c\n", character | 0x20);
 
     return 0;
